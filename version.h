e082460798b43361050d1d8d50f8a81bcdee69c4
Cavaler <cavaler.pk@gmail.com>
2013-03-01 15:50:03 +0000

    7.65 (the only change is in FarLib)

--- version.h
+++ version.h
@@ -2,6 +2,6 @@
 #define PLUGIN_DESCRIPTION "Regular Expression Search and Replace"
 #define PLUGIN_AUTHOR "Pavel Kostromitinov"
 #define PLUGIN_VERSION_MAJOR 7
-#define PLUGIN_VERSION_MINOR 64
+#define PLUGIN_VERSION_MINOR 65
 #define PLUGIN_VERSION_REVISION 0
-#define PLUGIN_VERSION_STRING "7, 64, 0, 0"
+#define PLUGIN_VERSION_STRING "7, 65, 0, 0"
