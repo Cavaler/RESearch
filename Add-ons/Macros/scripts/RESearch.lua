6131e1195fc32efa6a2d4f7c2abb1b3665f8f227
Cavaler <cavaler.pk@gmail.com>
2013-03-05 08:59:42 +0000

    Plugin.Call() - based macros

--- Add-ons/Macros/scripts/RESearch.lua
+++ Add-ons/Macros/scripts/RESearch.lua
@@ -1,59 +1,69 @@
 Macro {
-  description=""; area="Editor"; key="CtrlF7"; flags="";
-  action=function() Keys("F11 s r") end;
-}
-
-Macro {
-  description=""; area="Editor"; key="F7"; flags="";
-  action=function() Keys("F11 s s") end;
-}
-
-Macro {
-  description=""; area="Editor"; key="ShiftF7"; flags="";
-  action=function() Keys("F11 s a") end;
-}
-
-Macro {
-  description=""; area="Shell"; key="AltAdd"; flags="";
-  action=function() Keys("F11 s l") end;
-}
-
-Macro {
-  description=""; area="Shell"; key="AltF6"; flags="";
-  action=function() Keys("F11 s m") end;
-}
-
-Macro {
   description=""; area="Shell"; key="AltF7"; flags="";
-  action=function() Keys("F11 s s") end;
-}
-
-Macro {
-  description=""; area="Shell"; key="AltMultiply"; flags="";
-  action=function() Keys("F11 s f") end;
-}
-
-Macro {
-  description=""; area="Shell"; key="AltSubtract"; flags="";
-  action=function() Keys("F11 s u") end;
-}
-
-Macro {
-  description=""; area="Shell"; key="CtrlAltF6"; flags="";
-  action=function() Keys("F11 s n") end;
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 0) end;
 }
 
 Macro {
   description=""; area="Shell"; key="ShiftF7"; flags="";
-  action=function() Keys("F11 s r") end;
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 1) end;
 }
 
 Macro {
+  description=""; area="Shell"; key="CtrlAltF7"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 2) end;
+}
+
+Macro {
+  description=""; area="Shell"; key="AltF6"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 9) end;
+}
+
+Macro {
+  description=""; area="Shell"; key="CtrlAltF6"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 8) end;
+}
+
+Macro {
+  description=""; area="Shell"; key="AltAdd"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 4) end;
+}
+
+Macro {
+  description=""; area="Shell"; key="AltSubtract"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 5) end;
+}
+
+Macro {
+  description=""; area="Shell"; key="AltMultiply"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 6) end;
+}
+
+
+
+
+Macro {
+  description=""; area="Editor"; key="F7"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 100) end;
+}
+
+Macro {
+  description=""; area="Editor"; key="CtrlF7"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 101) end;
+}
+
+Macro {
+  description=""; area="Editor"; key="ShiftF7"; flags="";
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 105) end;
+}
+
+
+
+Macro {
   description=""; area="Viewer"; key="F7"; flags="";
-  action=function() Keys("F11 s s") end;
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 200) end;
 }
 
 Macro {
   description=""; area="Viewer"; key="ShiftF7"; flags="";
-  action=function() Keys("F11 s a") end;
+  action=function() Plugin.Call("F250C12A-78E2-4ABC-A784-3FDD3156E415", 201) end;
 }
