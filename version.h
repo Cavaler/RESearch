1c29937cb73ddc7e754d159b45e017e4009cc3a2
Cavaler <cavaler.pk@gmail.com>
2013-11-15 07:16:20 +0000

    7.84

--- version.h
+++ version.h
@@ -2,6 +2,6 @@
 #define PLUGIN_DESCRIPTION "Regular Expression Search and Replace"
 #define PLUGIN_AUTHOR "Pavel Kostromitinov"
 #define PLUGIN_VERSION_MAJOR 7
-#define PLUGIN_VERSION_MINOR 83
+#define PLUGIN_VERSION_MINOR 84
 #define PLUGIN_VERSION_REVISION 0
-#define PLUGIN_VERSION_STRING "7, 83, 0, 0"
+#define PLUGIN_VERSION_STRING "7, 84, 0, 0"
