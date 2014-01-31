dbbc20dbaa2505b14e30e6b31080aa20cdc14a89
Cavaler <cavaler.pk@gmail.com>
2014-01-31 12:28:13 +0000

    8.25

--- Update.bat
+++ Update.bat
@@ -1,5 +1,6 @@
 @echo off
 call UpdateVersion.bat
-rar f -r %@FINDFIRST[RESearch8*.rar]
-rar u -r %@FINDFIRST[RESearch8*.rar] Add-ons
+ren %@FINDFIRST[RESearch%V1%*.rar] RESearch%V1%%V2%%V3%.rar
+rar f -r %@FINDFIRST[RESearch%V1%*.rar]
+rar u -r %@FINDFIRST[RESearch%V1%*.rar] Add-ons
 rar f RESearchPDB.rar
